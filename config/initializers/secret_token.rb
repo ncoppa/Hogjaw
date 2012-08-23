fe7e64f9156261bbdd900a0b390a847b4484ca74
Nicole Coppa <coppa.nicole@gmail.com>
2012-08-23 15:20:36 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hogjaw::Application.config.secret_token = 'f4d2ecc2237d92b45ddf3209dd226582b7f30bd7b337f1a8da72c38ebf4269f690ad9066a10e369093eed4104f460fd6579c0b335ea2196eaba7d6af3727bf60'
