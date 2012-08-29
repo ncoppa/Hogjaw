6d48838617b53da1456eb61d1b99dee8cad9a991
Nicole Coppa <coppa.nicole@gmail.com>
2012-08-28 21:13:56 -0700

    init

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,25 @@
 class StaticController < ApplicationController
   def index
   end
+  
+  def our_music
+  end
+  
+  def history
+  end
+  
+  def photo
+  end
+  
+  def merchandise
+  end
 
   def tour_calendar
+  end
+  
+  def contact
+  end
+  
+  def press
   end
 end
